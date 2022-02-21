3c4c00c371bc1662ed28fe7b6af1a092269df70e
Amadeus Hoffmann <amadeusmaximilian@gmail.com>
2022-02-21 16:30:44 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'user#index'
+  root to: 'offers#index'
 
-  get 'index', to: 'user#index'
-  
+  get '/offers', to: 'offers#index', as: :offers # in link to helper say offers_path
+  get '/offers/:id', to: 'offers#view', as: :offer
+  post '/offers/:offer_id/bookings', to: 'bookings#create', as: :offer_booking
+  delete '/bookings/:id', to: 'bookings#destroy', as: :booking
+  get '/offers/new', to: 'offers#new', as: :new_offer
+  post '/offers', to: 'offers#create'
+  get '/offers/:id', to:  'offers#edit', as: :update_offer
+  patch '/offers/:id', to: 'offers#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
