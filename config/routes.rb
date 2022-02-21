1f4e7069e743967b8894a2e49c25dbe71582fded
Amadeus Hoffmann <amadeusmaximilian@gmail.com>
2022-02-21 15:56:57 +0100

    installed device

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'user#index'
+
+  get 'index', to: 'user#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
