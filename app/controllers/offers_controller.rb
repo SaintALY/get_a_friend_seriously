962f865a5819ac260f725cd79d3b6dd4d44ec7b0
Amadeus Hoffmann <amadeusmaximilian@gmail.com>
2022-02-22 12:48:14 +0100

    added controler and device

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,25 @@
 class OffersController < ApplicationController
+    def index
+        @offer = Offer.all
+    end
+
+    def new
+        @offer = Offer.new
+    end
+
+    def create
+        @offer = Offer.new(offer_params)
+        @offer.save
+        redirect_to_offer_path(@offer)
+    end
+
+    def show
+        @offer = Offer.find(params[:id])
+    end
+
+    private
+
+    def offer_params
+        params.require(:offer).permit(:title, :content, :wiki_url, :wiki_tag)
+    end
 end
