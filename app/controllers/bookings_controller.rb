8cc17a6e0034728785738fd69a8f3b72789f7583
Amadeus Hoffmann <amadeusmaximilian@gmail.com>
2022-02-23 15:02:36 +0100

    fixed the double display of notice message

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,7 +5,7 @@ class BookingsController < ApplicationController
         @booking.offer = @offer
         @booking.user = current_user
         if @booking.save
-            redirect_to offer_path(@offer), alert: "Yeah you booked a Friend"
+            redirect_to offer_path(@offer), alert: "Yeah, you booked a Friend."
         else
             render "offers/show"
         end
