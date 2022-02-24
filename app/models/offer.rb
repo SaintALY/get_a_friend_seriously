82e172db0a73b467e7a6412383f9e13c57f35f9b
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-02-24 14:55:50 +0100

    added geocode validation

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,5 +3,5 @@ class Offer < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
   geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, if: :will_save_change_to_location?
 end
